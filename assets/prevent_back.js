792cd40d53ee67552f4af044a50e96679b442909
elia <orezaeia@stevens.edu>
2023-10-24 19:28:11 -0400

    new changes

--- assets/prevent_back.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function(){
-    window.history.pushState(null, null, window.location.href);
-    window.onpopstate = function(event) {
-        window.history.go(1);
-    };
-});
